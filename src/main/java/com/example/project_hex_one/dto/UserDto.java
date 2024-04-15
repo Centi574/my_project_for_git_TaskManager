8fd009e183ba2c338fbe162ecebb050343e1efc3
Konstantin Zagorulya <k.zagorulya@mail.ru>
2024-04-15 18:12:45 +0600

    Add dto

---
+++ src/main/java/com/example/project_hex_one/dto/UserDto.java
@@ -0,0 +1,30 @@
+package com.example.project_hex_one.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 1)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 1)
+    private String lastName;
+
+    @NotBlank
+    @Size(min = 3)
+    private String password;
+}
