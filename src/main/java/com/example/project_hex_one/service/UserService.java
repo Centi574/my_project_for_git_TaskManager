de017fd35eb13869c9ce2d4b6b14783e8e56e091
Konstantin Zagorulya <k.zagorulya@mail.ru>
2024-04-15 18:42:39 +0600

    Add UserService

---
+++ src/main/java/com/example/project_hex_one/service/UserService.java
@@ -0,0 +1,53 @@
+package com.example.project_hex_one.service;
+
+import com.example.project_hex_one.dto.UserDto;
+import com.example.project_hex_one.exception.UserNotFoundException;
+import com.example.project_hex_one.model.User;
+import com.example.project_hex_one.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User createNewUser(UserDto userDto) {
+        User user = new User();
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+        return userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User getUserById(long id) {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found"));
+    }
+
+    @Transactional(readOnly = true)
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Transactional
+    public User updateUser(long id, UserDto userDto) {
+        final User user = userRepository.findById(id).get();
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public void deleteById(long id) {
+        userRepository.deleteById(id);
+    }
+}
