87c3a4d378ec8334294b6ae7ebbef99742aa60e7
Konstantin Zagorulya <k.zagorulya@mail.ru>
2024-04-15 17:46:58 +0600

    initial commit

---
+++ src/main/java/com/example/project_hex_one/BaseExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.example.project_hex_one;
+
+public class BaseExceptionHandler {
+}
---
+++ src/main/java/com/example/project_hex_one/ProjectHexOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_hex_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHexOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHexOneApplication.class, args);
+	}
+
+}
