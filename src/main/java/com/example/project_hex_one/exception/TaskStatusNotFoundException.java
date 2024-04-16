facd3a0c300264617194d25add27a51f682e8032
Konstantin Zagorulya <k.zagorulya@mail.ru>
2024-04-16 22:22:42 +0600

    Add Task Status Exception

---
+++ src/main/java/com/example/project_hex_one/exception/TaskStatusNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.project_hex_one.exception;
+
+public class TaskStatusNotFoundException extends RuntimeException {
+
+    public TaskStatusNotFoundException(String message) {
+        super(message);
+    }
+}
