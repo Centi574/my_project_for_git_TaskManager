1652aaff73e8d82145296fba43138494fdd781b2
Konstantin Zagorulya <k.zagorulya@mail.ru>
2024-04-16 22:19:44 +0600

    Add TaskStatusService

---
+++ src/main/java/com/example/project_hex_one/service/TaskStatusService.java
@@ -0,0 +1,35 @@
+package com.example.project_hex_one.service;
+
+import com.example.project_hex_one.dto.TaskStatusDto;
+import com.example.project_hex_one.model.TaskStatus;
+import com.example.project_hex_one.repository.TaskRepository;
+import com.example.project_hex_one.repository.TaskStatusRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class TaskStatusService {
+
+    private final TaskStatusRepository taskStatusRepository;
+
+    public TaskStatus createNewTaskStatus(TaskStatusDto taskStatusDto) {
+        TaskStatus taskStatus = new TaskStatus();
+        taskStatus.setName(taskStatusDto.getName());
+        return taskStatusRepository.save(taskStatus);
+    }
+
+    @Transactional
+    public TaskStatus updateTaskStatus(Long id, TaskStatusDto taskStatusDto) {
+        TaskStatus taskStatus = taskStatusRepository.findById(id).orElseThrow();
+        taskStatus.setName(taskStatusDto.getName());
+        return taskStatus;
+    }
+
+    @Transactional
+    public TaskStatus getTaskStatusById(Long id) {
+        return taskStatusRepository.findById(id).orElseThrow();
+    }
+}
