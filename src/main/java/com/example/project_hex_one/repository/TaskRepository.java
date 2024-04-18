ec907e4cfbea512f58c8ba9fa4b46a11b82f8e85
Konstantin Zagorulya <k.zagorulya@mail.ru>
2024-04-18 23:42:14 +0600

    Add custom method in Repository

--- src/main/java/com/example/project_hex_one/repository/TaskRepository.java
+++ src/main/java/com/example/project_hex_one/repository/TaskRepository.java
@@ -1,7 +1,11 @@
 package com.example.project_hex_one.repository;
 
 import com.example.project_hex_one.model.Task;
+import com.querydsl.core.types.Predicate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findAll(Predicate predicate);
 }
