2d5582041b22e9161632bb6c225bc3baa86bb42f
Konstantin Zagorulya <k.zagorulya@mail.ru>
2024-04-20 23:35:07 +0600

    Add swagger annotations

--- src/main/java/com/example/project_hex_one/controller/TaskController.java
+++ src/main/java/com/example/project_hex_one/controller/TaskController.java
@@ -23,29 +23,57 @@ public class TaskController {
     public static final String TASK_CONTROLLER_PATH = "/tasks";
     public static final String ID = "/{id}";
 
+    private static final String OWNER =
+            "@taskRepository.findById(#id).get().getAuthor().getEmail() == authentication.getName()";
+
     private final TaskService taskService;
     private final TaskRepository taskRepository;
 
+    @Operation(summary = "Create new task")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Task has been created",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Task.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content)})
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping()
     public Task createNewTask(@RequestBody TaskDto taskDto) {
         return taskService.createNewTask(taskDto);
     }
 
+    @Operation(summary = "Get all tasks")
+    @ApiResponse(responseCode = "200", description = "All tasks are found",
+            content = @Content(schema = @Schema(implementation = Task.class)))
     @GetMapping()
     public Iterable<Task> getAllTasks(@QuerydslPredicate Predicate predicate) {
         return predicate == null ? taskService.getAllTasks() : taskService.getAllTasks(predicate);
     }
 
+    @Operation(summary = "Get task by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The task is found",
+                    content = {@Content(mediaType = "application/jsom", schema = @Schema(implementation = Task.class))}),
+            @ApiResponse(responseCode = "404", description = "No such task found", content = @Content)})
     @GetMapping(path = ID)
     public Task getTaskById(@PathVariable(name = "id") Long id) {
         return taskService.getTaskById(id);
     }
 
+    @Operation(summary = "Update the task by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The task is successfully updated",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Task.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content)})
     @PutMapping(path = ID)
     public Task updateTask(@PathVariable(name = "id") Long id, @RequestBody TaskDto taskDto) {
         return taskService.updateTask(id, taskDto);
     }
 
+    @Operation(summary = "Delete the task by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The task has been successfully deleted"),
+            @ApiResponse(responseCode = "404", description = "The task is not found", content = @Content)})
+    @PreAuthorize(OWNER)
     @DeleteMapping(path = ID)
     public void deleteTask(@PathVariable(name = "id") Long id) {
         taskRepository.deleteById(id);
